cd8573f28975f767e19ff40d7066dc20325b45fc
Taylor <taylorhosk@hotmail.co.uk>
2023-10-19 19:15:03 +0100

    Initial commit

---
+++ src/main/java/net/taylor/tazportablejukebox/TazPortableJukeboxClient.java
@@ -0,0 +1,10 @@
+package net.taylor.tazportablejukebox;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TazPortableJukeboxClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
